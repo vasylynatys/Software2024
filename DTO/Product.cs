88d6d0723ec2c2f78fdab5b480ecd08cd92a0e3c
vasylynatys <linatys.024@gmail.com>
2024-11-26 14:45:49 +0200

    Add files via upload

---
+++ DTO/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int SupplierId { get; set; }
+    }
+}
---
+++ DTO/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set;}
+        public bool IsBlocked { get; set; }
+    }
+}
